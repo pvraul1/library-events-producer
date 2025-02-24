9cc504636c610583ab7d0c754f9c026be118e8d1
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-02-24 21:36:31 +0100

    42. Create the PUT endpoint - "/libraryEvent"

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -2,6 +2,7 @@ package com.learnkafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
 import com.learnkafka.producer.LibraryEventsProducer;
 import jakarta.validation.Valid;
 import java.util.concurrent.ExecutionException;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,9 +41,34 @@ public class LibraryEventsController {
         // invoke kafka producer
         libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
 
-        log.info("LibraryEvent sent to Kafka");
+        log.info("PostLibraryEvent sent to Kafka");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> updateLibraryEvent(@RequestBody @Valid final LibraryEvent libraryEvent)
+            throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
+        log.info("UpdateLibraryEvent: {}", libraryEvent);
+
+        ResponseEntity<String> BAD_REQUEST = this.validateLibraryEvent(libraryEvent);
+        if (BAD_REQUEST != null) return BAD_REQUEST;
+
+        // invoke kafka producer
+        libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
+
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
+    }
+
+    private static ResponseEntity<String> validateLibraryEvent(LibraryEvent libraryEvent) {
+        if (libraryEvent.libraryEventId() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the LibraryEventId");
+        }
+
+        if (!libraryEvent.libraryEventType().equals(LibraryEventType.UPDATE)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only UPDATE LibraryEvent is supported");
+        }
+        return null;
+    }
 }
